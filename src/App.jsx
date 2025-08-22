7f970c37fa5655fb8aaa97f4e4b9ecb605cd6b80
Hrusikesh123 <hrushikeshs113@gmail.com>
2025-08-23 01:04:46 +0530

    first-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
+import About from './sections/About'
 
 const App = () => {
   return (
@@ -8,6 +9,11 @@ const App = () => {
       {/* navbar */}
       <Navbar />
       <Hero />
+      <About />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
     </div>
   )
 }
--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -52,16 +52,40 @@ const HeroText = () => {
       </div>
         {/* Mobile View */}
       <div className="flex- flex-col space-y-6 md:hidden">
-        <motion.p className="text-4xl font-medium">Hi,I'm Hrusikesh</motion.p>
+        <motion.p className="text-4xl font-medium" 
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{ delay: 1 }}
+        >Hi,I'm Hrusikesh</motion.p>
         <motion.div>
-            <motion.p className="text-5xl font-black text-neutral-300">Building</motion.p>
-            <motion.div>
+            <motion.p className="text-5xl font-black text-neutral-300"
+                variants={variants}
+                initial="hidden"
+                animate="visible"
+                transition={{ delay: 1.2 }}
+            > 
+                Building
+            </motion.p>
+            <motion.div 
+                variants={variants}
+                initial="hidden"
+                animate="visible"
+                transition={{ delay: 1.5 }}
+            >
                 <FlipWords 
                     words={words}
                     className="font-bold text-white text-7xl" 
                 />
             </motion.div>
-            <motion.p className="text-4xl font-black text-neutral-300">Web Applications</motion.p>
+            <motion.p className="text-4xl font-black text-neutral-300"
+                variants={variants}
+                initial="hidden"
+                animate="visible"
+                transition={{ delay: 1.8 }}
+            >
+                Web Applications
+            </motion.p>
         </motion.div>
       </div>
     </div>
